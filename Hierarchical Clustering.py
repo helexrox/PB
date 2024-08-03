c167e0142d293ab3b692f89971e199e088186c8c
helexrox <ankithtech2003@gmail.com>
2024-08-03 19:55:04 +0530

    Update Hierarchical Clustering.py

--- Hierarchical Clustering.py
+++ Hierarchical Clustering.py
@@ -1,10 +1,8 @@
 from sklearn.cluster import AgglomerativeClustering
 import numpy as np
 
-# Example data
 X = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
 
-# Model
 model = AgglomerativeClustering(n_clusters=2)
 labels = model.fit_predict(X)
 
