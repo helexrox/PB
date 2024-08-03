f2da707407802abce9344ced59332cc45c8c6835
helexrox <ankithtech2003@gmail.com>
2024-08-03 19:54:25 +0530

    Update  Principal Component Analysis (PCA).py

--- Principal Component Analysis (PCA).py
+++ Principal Component Analysis (PCA).py
@@ -1,10 +1,8 @@
 from sklearn.decomposition import PCA
 import numpy as np
 
-# Example data
 X = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
 
-# Model
 pca = PCA(n_components=1)
 X_reduced = pca.fit_transform(X)
 
