1fad50b542e4ee00d4e77fb016e2386ba4e453d0
helexrox <ankithtech2003@gmail.com>
2024-08-03 19:52:27 +0530

    Update Boosting .py

--- Boosting .py
+++ Boosting .py
@@ -2,14 +2,11 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 
-# Example data
 X = np.array([[1], [2], [3], [4], [5]])
 y = np.array([0, 0, 1, 1, 1])
 
-# Model
-model = AdaBoostClassifier(base_estimator=DecisionTreeClassifier(), n_estimators=10)
+model = AdaBoostClassifier(estimator=DecisionTreeClassifier(), n_estimators=10)
 model.fit(X, y)
 
-# Predictions
 predictions = model.predict(X)
 print("Predictions:", predictions)
