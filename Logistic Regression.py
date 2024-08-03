bedb8b3c1ed084f1b981e0f196e0705fc0e368a6
helexrox <ankithtech2003@gmail.com>
2024-08-03 19:55:29 +0530

    Update Logistic Regression.py

--- Logistic Regression.py
+++ Logistic Regression.py
@@ -1,14 +1,11 @@
 from sklearn.linear_model import LogisticRegression
 import numpy as np
 
-# Example data
 X = np.array([[1], [2], [3], [4], [5]])
 y = np.array([0, 0, 1, 1, 1])
 
-# Model
 model = LogisticRegression()
 model.fit(X, y)
 
-# Predictions
 predictions = model.predict(X)
 print("Predictions:", predictions)
