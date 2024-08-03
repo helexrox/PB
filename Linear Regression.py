1895513841dbaf5cfc1c4d5c574a570660524646
helexrox <ankithtech2003@gmail.com>
2024-08-03 19:55:16 +0530

    Update Linear Regression.py

--- Linear Regression.py
+++ Linear Regression.py
@@ -1,14 +1,11 @@
 from sklearn.linear_model import LinearRegression
 import numpy as np
 
-# Example data
 X = np.array([[1], [2], [3], [4], [5]])
 y = np.array([1, 3, 3, 2, 5])
 
-# Model
 model = LinearRegression()
 model.fit(X, y)
 
-# Predictions
 predictions = model.predict(X)
 print("Predictions:", predictions)
